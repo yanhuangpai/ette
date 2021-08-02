3026e9528d4af0fdc5cd435492cb341e1cc8ee09
yanhuangpai <yanhuangpai@126.com>
2021-08-03 01:34:45 +0800

    backup db

--- db/ette.sql
+++ db/ette.sql
@@ -3,13 +3,19 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主机： localhost
--- 生成日期： 2021-08-02 14:31:34
+-- 生成日期： 2021-08-02 17:32:27
 -- 服务器版本： 5.7.34
 -- PHP 版本： 7.3.24-(to be removed in future macOS)
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- 数据库： `ette`
@@ -36,7 +42,20 @@ CREATE TABLE `blocks` (
   `unclehash` char(66) NOT NULL,
   `txroothash` char(66) NOT NULL,
   `receiptroothash` char(66) NOT NULL,
-  `extradata` blob
+  `extradata` blob,
+  `inputdata` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `config_vars`
+--
+
+CREATE TABLE `config_vars` (
+  `id` int(11) NOT NULL,
+  `name` varchar(25) NOT NULL,
+  `value` varchar(125) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -140,6 +159,12 @@ ALTER TABLE `blocks`
   ADD KEY `idx_blocks_time` (`time`);
 
 --
+-- 表的索引 `config_vars`
+--
+ALTER TABLE `config_vars`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- 表的索引 `events`
 --
 ALTER TABLE `events`
@@ -183,8 +208,18 @@ ALTER TABLE `users`
 --
 
 --
+-- 使用表AUTO_INCREMENT `config_vars`
+--
+ALTER TABLE `config_vars`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- 使用表AUTO_INCREMENT `subscription_plans`
 --
 ALTER TABLE `subscription_plans`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
