5f9334c86162a361e07dfe168a0ecc3512f6c4db
yanhuangpai <yanhuangpai@126.com>
2021-08-02 14:22:02 +0800

    i

--- app/db/model.go
+++ app/db/model.go
@@ -16,23 +16,23 @@ type Tabler interface {
 
 // Blocks - Mined block info holder table model
 type Blocks struct {
-	Hash                string       `gorm:"column:hash;type:char(66);primaryKey"`
-	Number              uint64       `gorm:"column:number;type:bigint;not null;unique;index:,sort:asc"`
-	Time                uint64       `gorm:"column:time;type:bigint;not null;index:,sort:asc"`
-	ParentHash          string       `gorm:"column:parenthash;type:char(66);not null"`
-	Difficulty          string       `gorm:"column:difficulty;type:varchar;not null"`
-	GasUsed             uint64       `gorm:"column:gasused;type:bigint;not null"`
-	GasLimit            uint64       `gorm:"column:gaslimit;type:bigint;not null"`
-	Nonce               string       `gorm:"column:nonce;type:varchar;not null"`
-	Miner               string       `gorm:"column:miner;type:char(42);not null"`
-	Size                float64      `gorm:"column:size;type:float(8);not null"`
-	StateRootHash       string       `gorm:"column:stateroothash;type:char(66);not null"`
-	UncleHash           string       `gorm:"column:unclehash;type:char(66);not null"`
-	TransactionRootHash string       `gorm:"column:txroothash;type:char(66);not null"`
-	ReceiptRootHash     string       `gorm:"column:receiptroothash;type:char(66);not null"`
-	ExtraData           []byte       `gorm:"column:extradata;type:bytea"`
-	Transactions        Transactions `gorm:"foreignKey:blockhash"`
-	Events              Events       `gorm:"foreignKey:blockhash"`
+	Hash                string  `gorm:"column:hash;type:char(66);primaryKey"`
+	Number              uint64  `gorm:"column:number;type:bigint;not null;unique;index:,sort:asc"`
+	Time                uint64  `gorm:"column:time;type:bigint;not null;index:,sort:asc"`
+	ParentHash          string  `gorm:"column:parenthash;type:char(66);not null"`
+	Difficulty          string  `gorm:"column:difficulty;type:varchar;not null"`
+	GasUsed             uint64  `gorm:"column:gasused;type:bigint;not null"`
+	GasLimit            uint64  `gorm:"column:gaslimit;type:bigint;not null"`
+	Nonce               string  `gorm:"column:nonce;type:varchar;not null"`
+	Miner               string  `gorm:"column:miner;type:char(42);not null"`
+	Size                float64 `gorm:"column:size;type:float(8);not null"`
+	StateRootHash       string  `gorm:"column:stateroothash;type:char(66);not null"`
+	UncleHash           string  `gorm:"column:unclehash;type:char(66);not null"`
+	TransactionRootHash string  `gorm:"column:txroothash;type:char(66);not null"`
+	ReceiptRootHash     string  `gorm:"column:receiptroothash;type:char(66);not null"`
+	ExtraData           []byte  `gorm:"column:extradata;type:bytea"`
+	// Transactions        Transactions `gorm:"foreignKey:blockhash"`
+	// Events              Events       `gorm:"foreignKey:blockhash"`
 }
 
 // TableName - Overriding default table name
@@ -73,7 +73,7 @@ type Transactions struct {
 	Nonce     uint64 `gorm:"column:nonce;type:bigint;not null;index"`
 	State     uint64 `gorm:"column:state;type:smallint;not null"`
 	BlockHash string `gorm:"column:blockhash;type:char(66);not null;index"`
-	Events    Events `gorm:"foreignKey:txhash"`
+	// Events    Events `gorm:"foreignKey:txhash"`
 }
 
 // TableName - Overriding default table name
@@ -156,10 +156,10 @@ func (DeliveryHistory) TableName() string {
 // SubscriptionPlans - Allowed subscription plans, to be auto populated from
 // .plans.json, at application start up
 type SubscriptionPlans struct {
-	ID                  uint32              `gorm:"column:id;type:serial;primaryKey" json:"id"`
-	Name                string              `gorm:"column:name;type:varchar(20);not null;unique" json:"name"`
-	DeliveryCount       uint64              `gorm:"column:deliverycount;type:bigint;not null;unique" json:"deliveryCount"`
-	SubscriptionDetails SubscriptionDetails `gorm:"foreignKey:subscriptionplan"`
+	ID            uint32 `gorm:"column:id;type:serial;primaryKey" json:"id"`
+	Name          string `gorm:"column:name;type:varchar(20);not null;unique" json:"name"`
+	DeliveryCount uint64 `gorm:"column:deliverycount;type:bigint;not null;unique" json:"deliveryCount"`
+	// SubscriptionDetails SubscriptionDetails `gorm:"foreignKey:subscriptionplan"`
 }
 
 // TableName - Overriding default table name
